d63d4120e32ad434c2f12c0f66a4c2782ae7db75
v.Hodobay <v.hodobay@yahoo.com>
2022-10-05 15:35:12 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'blue': '#1fb6ff',
+      'light-purple': '#2469A614',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  },
+  plugins: [],
+}
