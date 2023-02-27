52c24a0e4cd0f6e290078decc3ccd5aa7ce90d93
Pranav Mishra <pm475@cornell.edu>
2023-02-27 14:34:44 -0500

    created chatroom file, one error left

---
+++ src/chatroom.tsx
@@ -0,0 +1,173 @@
+import React, { useRef, useState } from 'react';
+// import './App.css';
+
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+// import 'firebase/analytics';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
+import { getFirestore } from "firebase/firestore"
+import { Query } from '@firebase/firestore-types'
+import { DocumentData } from '@firebase/firestore-types';
+
+
+
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { initializeApp } from 'firebase/app';
+// import { beforeAuthStateChanged } from 'react-firebase-hooks/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm_I31oPosmotAYcCRUANjOmtajNY_y7k",
+  authDomain: "chatroom-d4a89.firebaseapp.com",
+  projectId: "chatroom-d4a89",
+  storageBucket: "chatroom-d4a89.appspot.com",
+  messagingSenderId: "1001079042465",
+  appId: "1:1001079042465:web:9ba7a9a63c0b07888fdbe0"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+
+
+const auth = getAuth(app);
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
+export { db }
+function Chatroom() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef<null | HTMLDivElement>(null);
+  const messagesRef = firestore.collection('messages');
+  const ref = messagesRef.orderBy('createdAt').limit(25);
+
+  let query: Query<DocumentData> = ref;
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+
+    if (auth.currentUser) {
+      const { uid, photoURL } = auth.currentUser;
+
+
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL
+      })
+
+      setFormValue('');
+      if (dummy.current) {
+        dummy.current.scrollIntoView({ behavior: 'smooth' });
+      }
+      else {
+        console.log("dummy.current is null");
+      }
+
+
+
+    }
+    else {
+      console.log("user is null");
+    }
+  }
+
+
+  return (<>
+    <main>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+interface Props {
+  // onClick: () => void;
+  message: DocumentData;
+}
+
+function ChatMessage(props: Props) { //edit Props interface
+  const { text, uid, photoURL } = props.message;
+
+  if (auth.currentUser) {
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+
+    return (<>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+        <p>{text}</p>
+      </div>
+    </>)
+  }
+  else {
+    console.log("auth.currentuser is null");
+    return null;
+  }
+}
+
+
+export default Chatroom;
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,21 +1,49 @@
+// // Import the functions you need from the SDKs you need
+// import { getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAWCKEkLBpYgN_dIJl6ncEHPngL21j3kz4",
+//   authDomain: "okb-hope.firebaseapp.com",
+//   projectId: "okb-hope",
+//   storageBucket: "okb-hope.appspot.com",
+//   messagingSenderId: "321787147976",
+//   appId: "1:321787147976:web:190348bb9e96343a7b97fd",
+//   measurementId: "G-0EKC171E9X"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
+import { initializeApp, getApps, getApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+// TODO: Replace with your own Firebase config
 // Import the functions you need from the SDKs you need
-import { getApps, initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAWCKEkLBpYgN_dIJl6ncEHPngL21j3kz4",
-  authDomain: "okb-hope.firebaseapp.com",
-  projectId: "okb-hope",
-  storageBucket: "okb-hope.appspot.com",
-  messagingSenderId: "321787147976",
-  appId: "1:321787147976:web:190348bb9e96343a7b97fd",
-  measurementId: "G-0EKC171E9X"
+  apiKey: "AIzaSyCm_I31oPosmotAYcCRUANjOmtajNY_y7k",
+  authDomain: "chatroom-d4a89.firebaseapp.com",
+  projectId: "chatroom-d4a89",
+  storageBucket: "chatroom-d4a89.appspot.com",
+  messagingSenderId: "1001079042465",
+  appId: "1:1001079042465:web:9ba7a9a63c0b07888fdbe0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+export { db }
+
